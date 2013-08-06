b1669657e4c7d1e0ae910e7414c150f673a488e3
Tuomas Soikkeli <tuomas.m.soikkeli@stundent.jyu.fi>
2013-08-06 21:02:19 +0300

    Init.

---
+++ CRC16CCITT.java
@@ -0,0 +1,38 @@
+
+/*
+ * Class for calculating CRC16CITT for byte arrays.
+ * @author Tuomas Soikkeli 
+ */
+public class CRC16CCITT {
+	
+	public int crc = 0;
+	
+	public void calc_crc(int character) {
+		int crc_tmp;
+		crc_tmp = ((crc ^ character) & 0x0F);
+		crc = (crc >>> 4) ^ (crc_tmp * 4225);
+		crc_tmp = ((crc ^ (character >>> 4)) & 0x0F);
+		crc = (crc >>> 4) ^ (crc_tmp * 4225);
+	}
+	
+	public void update(byte b) {
+		calc_crc(b);
+	}
+	
+	public void reset() {
+		crc = 0;
+	}
+	
+	public int highByte() {
+		return (( crc >> 8) & 0xFF);
+
+	}
+	
+	public int lowByte() {
+		return crc & 0xFF;
+	}
+	
+	public int value() {
+		return crc;
+	}
+}
